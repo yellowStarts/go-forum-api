9d87b128b5842097b9e0dd11ab2c9c4034b35329
huanggz <918031078@qq.com>
2022-01-07 15:19:40 +0800

    init route

---
+++ bootstrap/route.go
@@ -0,0 +1,53 @@
+// Package bootstrap 处理程序初始化逻辑
+package bootstrap
+
+import (
+	"huango/routes"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+// SetupRoute 路由初始化
+func SetupRoute(router *gin.Engine) {
+	// 注册全局中间件
+	registerGlobalMiddleware(router)
+
+	// 注册 API 路由
+	routes.RegisterAPIRoutes(router)
+
+	// 注册 Admin 路由
+	routes.RegisterAdminRoutes(router)
+
+	// 注册 Seller 路由
+	routes.RegisterSellerRoutes(router)
+
+	// 配置 404 路由
+	setup404Handler(router)
+}
+
+func registerGlobalMiddleware(router *gin.Engine) {
+	router.Use(
+		gin.Logger(),
+		gin.Recovery(),
+	)
+}
+
+func setup404Handler(router *gin.Engine) {
+	// 处理 404 请求
+	router.NoRoute(func(c *gin.Context) {
+		// 获取标头信息的 Accepte 信息
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			// 如果是 HTML 的话
+			c.String(http.StatusNotFound, "页面返回 404")
+		} else {
+			// 默认返回 JSON
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定义，请确认 url 和请求方法是否正确",
+			})
+		}
+	})
+}
