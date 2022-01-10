ae5edb3f6210f3556c4e8c37985fd81870fef551
huanggz <918031078@qq.com>
2022-01-10 09:46:47 +0800

    phone and verifycode registe api

--- app/http/requests/signup_request.go
+++ app/http/requests/signup_request.go
@@ -2,6 +2,8 @@
 package requests
 
 import (
+	"huango/app/http/requests/validators"
+
 	"github.com/gin-gonic/gin"
 	"github.com/thedevsaddam/govalidator"
 )
@@ -29,7 +31,6 @@ func SignupPhoneExist(data interface{}, c *gin.Context) map[string][]string {
 	return validate(data, rules, messages)
 }
 
-
 func SignupEmailExist(data interface{}, c *gin.Context) map[string][]string {
 	// 自定义验证规则
 	rules := govalidator.MapData{
@@ -45,4 +46,54 @@ func SignupEmailExist(data interface{}, c *gin.Context) map[string][]string {
 		},
 	}
 	return validate(data, rules, messages)
+}
+
+// SignupUsingPhoneRequest 通过手机号注册的请求信息
+type SignupUsingPhoneRequest struct {
+	Phone           string `json:"phone,omitempty" valid:"phone"`
+	VerifyCode      string `json:"verify_code,omitempty" valid:"verify_code"`
+	Name            string `json:"name" valid:"name"`
+	Password        string `json:"password,omitempty" valid:"password"`
+	PasswordConfirm string `json:"password_confirm,omitempty" valid:"password_confirm"`
+}
+
+func SignupUsingPhone(data interface{}, c *gin.Context) map[string][]string {
+	rules := govalidator.MapData{
+		"phone":            []string{"required", "digits:11", "not_exists:users,phone"},
+		"name":             []string{"required", "alpha_num", "between:3,20"},
+		"password":         []string{"required", "min:6"},
+		"password_confirm": []string{"required"},
+		"verify_code":      []string{"required", "digits:6"},
+	}
+
+	message := govalidator.MapData{
+		"phone": []string{
+			"required:手机号为必填项，参数名称 phone",
+			"digits:手机号长度必须为 11 位的数字",
+		},
+		"name": []string{
+			"required:用户名为必填项",
+			"alpha_num:用户名格式错误，只允许数字和英文",
+			"between:用户名长度需在 3~20 之间",
+		},
+		"password": []string{
+			"required:密码为必填项",
+			"min:密码长度需大于 6",
+		},
+		"password_confirm": []string{
+			"required:确认密码为必填项",
+		},
+		"verify_code": []string{
+			"required:验证码答案必填",
+			"digits:验证码长度必须为 6 位的数字",
+		},
+	}
+
+	errs := validate(data, rules, message)
+
+	_data := data.(*SignupUsingPhoneRequest)
+	errs = validators.ValidatePasswordConfirm(_data.Password, _data.PasswordConfirm, errs)
+	errs = validators.ValidateVerifyCode(_data.Phone, _data.VerifyCode, errs)
+
+	return errs
 }
--- routes/api.go
+++ routes/api.go
@@ -20,6 +20,8 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
 			// 判断邮箱是否注册
 			authGroup.POST("/signup/email/exist", suc.IsEmailExist)
+			// 手机 + 验证码 注册
+			authGroup.POST("/signup/using-phone", suc.SignupUsingPhone)
 
 			// 发送验证码
 			vcc := new(auth.VerifyCodeController)
