dd3d22a73c0a5436263b6a291fd39ed71c3bbcf5
huanggz <918031078@qq.com>
2022-01-11 14:51:46 +0800

    add migrate up command

--- app/cmd/migrate.go
+++ app/cmd/migrate.go
@@ -18,6 +18,12 @@ var CmdMigrate = &cobra.Command{
 	// 所有 migrate 下的子命令都会执行以下代码
 }
 
+var CmdMigrateUp = &cobra.Command{
+    Use:   "up",
+    Short: "Run unmigrated migrations",
+    Run:   runUp,
+}
+
 func init() {
 	CmdMigrate.AddCommand(
 		CmdMigrateUp,
