ccc4a22658fe8be2721374d2078ed84b80228325
huanggz <918031078@qq.com>
2022-01-12 10:37:22 +0800

    delete category

--- routes/api.go
+++ routes/api.go
@@ -84,6 +84,8 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			cgcGroup.POST("", middlewares.AuthJWT(), cgc.Store)
 			// 更新分类
 			cgcGroup.PUT("/:id", middlewares.AuthJWT(), cgc.Update)
+			// 删除分类
+			cgcGroup.DELETE("/:id", middlewares.AuthJWT(), cgc.Delete)
 		}
 
 		// 测试路由
