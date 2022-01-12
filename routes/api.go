bb8f71282512c1fe1cdc526b10464886b450524d
huanggz <918031078@qq.com>
2022-01-12 11:30:30 +0800

    update topic

--- routes/api.go
+++ routes/api.go
@@ -92,7 +92,10 @@ func RegisterAPIRoutes(r *gin.Engine) {
 		tpc := new(controllers.TopicsController)
 		tpcGroup := v1.Group("/topics")
 		{
+			// 新建话题
 			tpcGroup.POST("", middlewares.AuthJWT(), tpc.Store)
+			// 更新话题
+			tpcGroup.PUT("/:id", middlewares.AuthJWT(), tpc.Update)
 		}
 
 		// 测试路由
