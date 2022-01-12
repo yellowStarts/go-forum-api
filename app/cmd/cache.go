7caab792b55c62301b5b8e27313f88430e0eefcd
huanggz <918031078@qq.com>
2022-01-12 16:15:05 +0800

    cacheforget command

--- app/cmd/cache.go
+++ app/cmd/cache.go
@@ -1,10 +1,11 @@
 package cmd
 
 import (
-    "huango/pkg/cache"
-    "huango/pkg/console"
+	"fmt"
+	"huango/pkg/cache"
+	"huango/pkg/console"
 
-    "github.com/spf13/cobra"
+	"github.com/spf13/cobra"
 )
 
 var CmdCache = &cobra.Command{
@@ -18,12 +19,29 @@ var CmdCacheClear = &cobra.Command{
     Run:   runCacheClear,
 }
 
+var CmdCacheForget = &cobra.Command{
+    Use:   "forget",
+    Short: "Delete redis key, example: cache forget cache-key",
+    Run:   runCacheForget,
+}
+
+// forget 命令的选项
+var cacheKey string
+
 func init() {
     // 注册 cache 命令的子命令
-    CmdCache.AddCommand(CmdCacheClear)
+    CmdCache.AddCommand(CmdCacheClear, CmdCacheForget)
+
+	// 设置 cache forget 命令的选项
+	CmdCacheForget.Flags().StringVarP(&cacheKey, "key", "k", "", "KEY of the cache")
+    CmdCacheForget.MarkFlagRequired("key")
 }
 
 func runCacheClear(cmd *cobra.Command, args []string) {
     cache.Flush()
     console.Success("Cache cleared.")
+}
+func runCacheForget(cmd *cobra.Command, args []string) {
+    cache.Forget(cacheKey)
+    console.Success(fmt.Sprintf("Cache key [%s] deleted.", cacheKey))
 }
